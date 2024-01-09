ac2956cd0f61587ccd4bb3218042d155a220fd4d
martineho <martineolsenn@hotmail.com>
2024-01-09 08:28:33 -0500

    Update index.jsx

--- src/handlers/index.jsx
+++ src/handlers/index.jsx
@@ -1,5 +1,4 @@
 export * from "./handlePlotElementClick";
-export * from "./setSubplotPositioningFromDOM";
 export * from "./updateSubplotTitles";
 export * from "./handleZoom";
 export * from "./handleLegend";
